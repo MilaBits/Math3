cdaf1bd08e3e07161e0f43f569b6b1730fbeca8c
Milly <milabits@gmail.com>
2019-07-11 00:21:20 +0200

    fixed first startup bug where game had no theme

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 	[InlineEditor]
 	public Theme theme;
-	
-	
+
+	private void Awake()
+	{
+		PlayerPrefs.DeleteAll();
+		if (PlayerPrefs.GetString("Theme").IsNullOrWhitespace()) PlayerPrefs.SetString("Theme", "Default");
+	}
+
 	public void LoadScene(string scene)
 	{
 		SceneManager.LoadScene(scene);
